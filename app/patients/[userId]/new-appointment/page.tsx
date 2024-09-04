801065cb208d0874eecbbffc623430df64101a89
rafaella godinho <rafaellagodinho@outlook.com>
2024-09-04 10:12:50 -0300

    skeleton for newappointment

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,5 +1,34 @@
-const page = () => {
-  return <div>New Appointment</div>;
-};
+import { PatientForm } from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-export default page;
+export default function NewAppointment() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl::text-left">
+              © 2024 CarePulse
+            </p>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
