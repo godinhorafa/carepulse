3775c2ba2729bcb06b3754ef19ce456abb13e066
rafaella godinho <rafaellagodinho@outlook.com>
2024-09-05 20:50:42 -0300

    test for remove error

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -1,39 +1,31 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { Dispatch, SetStateAction, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { SelectItem } from "@/components/ui/select";
-import { Doctors } from "@/constants";
-import {
-  createAppointment,
-  updateAppointment,
-} from "@/lib/actions/appointment.actions";
+import { Form } from "@/components/ui/form";
+import { createUser } from "@/lib/actions/patient.actions";
 import { getAppointmentSchema } from "@/lib/validation";
-import { Appointment } from "@/types/appwrite.types";
 
-import "react-datepicker/dist/react-datepicker.css";
-
+import "react-phone-number-input/style.css";
 import CustomFormField, { FormFieldType } from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
-import { Form } from "../ui/form";
+import { Doctors } from "@/constants";
+import Image from "next/image";
+import { SelectItem } from "../ui/select";
+import { createAppointment } from "@/lib/actions/appointment.actions";
 
 export const AppointmentForm = ({
   userId,
   patientId,
-  type = "create",
-  appointment,
-  setOpen,
+  type,
 }: {
   userId: string;
   patientId: string;
-  type: "create" | "schedule" | "cancel";
-  appointment?: Appointment;
-  setOpen?: Dispatch<SetStateAction<boolean>>;
+  type: "create" | "cancel" | "schedule";
 }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -43,13 +35,11 @@ export const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: appointment ? appointment?.primaryPhysician : "",
-      schedule: appointment
-        ? new Date(appointment?.schedule!)
-        : new Date(Date.now()),
-      reason: appointment ? appointment.reason : "",
-      note: appointment?.note || "",
-      cancellationReason: appointment?.cancellationReason || "",
+      primaryPhysician: "",
+      schedule: new Date(),
+      reason: "",
+      note: "",
+      cancellationReason: "",
     },
   });
 
@@ -60,85 +50,69 @@ export const AppointmentForm = ({
 
     let status;
     switch (type) {
-      case "schedule":
-        status = "scheduled";
-        break;
       case "cancel":
         status = "cancelled";
         break;
+      case "schedule":
+        status = "scheduled";
+        break;
       default:
         status = "pending";
+        break;
     }
 
     try {
       if (type === "create" && patientId) {
-        const appointment = {
+        const appointmentData = {
           userId,
           patient: patientId,
           primaryPhysician: values.primaryPhysician,
           schedule: new Date(values.schedule),
           reason: values.reason!,
-          status: status as Status,
           note: values.note,
+          status: status as Status,
         };
 
-        const newAppointment = await createAppointment(appointment);
+        const appointment = await createAppointment(appointmentData);
 
-        if (newAppointment) {
+        if (appointment) {
           form.reset();
           router.push(
-            `/patients/${userId}/new-appointment/success?appointmentId=${newAppointment.$id}`
+            `/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`
           );
-        }
-      } else {
-        const appointmentToUpdate = {
-          userId,
-          appointmentId: appointment?.$id!,
-          appointment: {
-            primaryPhysician: values.primaryPhysician,
-            schedule: new Date(values.schedule),
-            status: status as Status,
-            cancellationReason: values.cancellationReason,
-          },
-          type,
-        };
-
-        const updatedAppointment = await updateAppointment(appointmentToUpdate);
-
-        if (updatedAppointment) {
-          setOpen && setOpen(false);
-          form.reset();
         }
       }
     } catch (error) {
       console.log(error);
     }
+
     setIsLoading(false);
   };
 
   let buttonLabel;
+
   switch (type) {
     case "cancel":
       buttonLabel = "Cancel Appointment";
+      break;
+    case "create":
+      buttonLabel = "Create Appointment";
       break;
     case "schedule":
       buttonLabel = "Schedule Appointment";
       break;
     default:
-      buttonLabel = "Submit Apppointment";
+      buttonLabel = "Request Appointment";
+      break;
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
-        {type === "create" && (
-          <section className="mb-12 space-y-4">
-            <h1 className="header">New Appointment</h1>
-            <p className="text-dark-700">
-              Request a new appointment in 10 seconds.
-            </p>
-          </section>
-        )}
+        <section className="mb-12 space-y-4">
+          <h1 className="header">New Appointment</h1>
+          <p className="text-dark-700">Request a new appointment</p>
+        </section>
 
         {type !== "cancel" && (
           <>
@@ -149,14 +123,14 @@ export const AppointmentForm = ({
               label="Doctor"
               placeholder="Select a doctor"
             >
-              {Doctors.map((doctor, i) => (
-                <SelectItem key={doctor.name + i} value={doctor.name}>
+              {Doctors.map((doctor) => (
+                <SelectItem key={doctor.name} value={doctor.name}>
                   <div className="flex cursor-pointer items-center gap-2">
                     <Image
                       src={doctor.image}
                       width={32}
                       height={32}
-                      alt="doctor"
+                      alt={doctor.name}
                       className="rounded-full border border-dark-500"
                     />
                     <p>{doctor.name}</p>
@@ -171,41 +145,39 @@ export const AppointmentForm = ({
               name="schedule"
               label="Expected appointment date"
               showTimeSelect
-              dateFormat="MM/dd/yyyy  -  h:mm aa"
+              dateFormat="MM/dd/yyyy h:mm aa"
             />
 
-            <div
-              className={`flex flex-col gap-6  ${type === "create" && "xl:flex-row"}`}
-            >
+            <div className="flex flex-col gap-6 xl:flex-row">
               <CustomFormField
                 fieldType={FormFieldType.TEXTAREA}
                 control={form.control}
                 name="reason"
-                label="Appointment reason"
-                placeholder="Annual montly check-up"
-                disabled={type === "schedule"}
+                label="Reason for appointment"
+                placeholder="Enter reason for appointment"
               />
 
               <CustomFormField
                 fieldType={FormFieldType.TEXTAREA}
                 control={form.control}
                 name="note"
-                label="Comments/notes"
-                placeholder="Prefer afternoon appointments, if possible"
-                disabled={type === "schedule"}
+                label="Notes"
+                placeholder="Enter notes"
               />
             </div>
           </>
         )}
 
         {type === "cancel" && (
-          <CustomFormField
-            fieldType={FormFieldType.TEXTAREA}
-            control={form.control}
-            name="cancellationReason"
-            label="Reason for cancellation"
-            placeholder="Urgent meeting came up"
-          />
+          <>
+            <CustomFormField
+              fieldType={FormFieldType.TEXTAREA}
+              control={form.control}
+              name="cancellationReason"
+              label="Cancellation reason"
+              placeholder="Enter cancellation reason"
+            />
+          </>
         )}
 
         <SubmitButton
